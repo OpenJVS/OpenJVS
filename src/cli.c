b5122be26e139bdb6ba81cd2beba86e130bfb3d8
Bobby Dilley <bobby@dilley.uk>
2020-10-07 11:57:48 +0100

    Add new CLI

---
+++ src/cli.c
@@ -0,0 +1,213 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cli.h"
+
+#include "config.h"
+#include "debug.h"
+
+JVSCLIStatus printUsage()
+{
+    debug(0, "Usage: openjvs ( options [controller] | [game] )\n\n");
+    debug(0, "Options:\n");
+    debug(0, "  --list     Lists all controllers\n");
+    debug(0, "  --enable   Enables a new/all controller(s)\n");
+    debug(0, "  --disable  Disables a new/all controller(s)\n");
+    debug(0, "  --help     Displays this text\n");
+    debug(0, "  --version  Displays the OpenJVS Version\n");
+    return JVS_CLI_STATUS_SUCCESS_CLOSE;
+}
+
+JVSCLIStatus printVersion()
+{
+    debug(0, "3.3.1\n");
+    return JVS_CLI_STATUS_SUCCESS_CLOSE;
+}
+
+JVSCLIStatus enableDevice(char *deviceName)
+{
+    if (!deviceName)
+    {
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(DEFAULT_DEVICE_MAPPING_PATH);
+        if (d)
+        {
+            while ((dir = readdir(d)) != NULL)
+            {
+                char gamePath[MAX_PATH_LENGTH];
+                strcpy(gamePath, DEFAULT_DEVICE_MAPPING_PATH);
+                strcat(gamePath, dir->d_name);
+
+                char gamePathEnabled[MAX_PATH_LENGTH];
+                strcpy(gamePathEnabled, gamePath);
+
+                for (int i = 0; i < MAX_PATH_LENGTH; i++)
+                {
+                    if (gamePathEnabled[i] == '.')
+                    {
+                        gamePathEnabled[i] = 0;
+                        break;
+                    }
+                }
+
+                rename(gamePath, gamePathEnabled);
+            }
+            closedir(d);
+        }
+
+        debug(0, "OpenJVS has enabled all controllers.\n");
+        return JVS_CLI_STATUS_SUCCESS_CLOSE;
+    }
+
+    char gamePath[MAX_PATH_LENGTH];
+    strcpy(gamePath, DEFAULT_DEVICE_MAPPING_PATH);
+    strcat(gamePath, deviceName);
+
+    char gamePathDisabled[MAX_PATH_LENGTH];
+    strcpy(gamePathDisabled, gamePath);
+    strcat(gamePathDisabled, ".disabled");
+
+    if (rename(gamePathDisabled, gamePath) < 0)
+    {
+        debug(0, "Failed to enable the controller, does it exist and is it already enabled?\n");
+        return JVS_CLI_STATUS_ERROR;
+    }
+
+    debug(0, "OpenJVS has enabled the controller '%s'.\n", deviceName);
+    return JVS_CLI_STATUS_SUCCESS_CLOSE;
+}
+
+JVSCLIStatus disableDevice(char *deviceName)
+{
+    if (!deviceName)
+    {
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(DEFAULT_DEVICE_MAPPING_PATH);
+        if (d)
+        {
+            while ((dir = readdir(d)) != NULL)
+            {
+                char gamePathEnabled[MAX_PATH_LENGTH];
+                strcpy(gamePathEnabled, DEFAULT_DEVICE_MAPPING_PATH);
+                strcat(gamePathEnabled, dir->d_name);
+
+                char gamePathDisabled[MAX_PATH_LENGTH];
+                strcpy(gamePathDisabled, gamePathEnabled);
+                strcat(gamePathDisabled, ".disabled");
+
+                rename(gamePathEnabled, gamePathDisabled);
+            }
+            closedir(d);
+        }
+
+        debug(0, "OpenJVS has disabled all controllers.\n");
+        return JVS_CLI_STATUS_SUCCESS_CLOSE;
+    }
+
+    char gamePath[MAX_PATH_LENGTH];
+    strcpy(gamePath, DEFAULT_DEVICE_MAPPING_PATH);
+    strcat(gamePath, deviceName);
+
+    char gamePathDisabled[MAX_PATH_LENGTH];
+    strcpy(gamePathDisabled, gamePath);
+    strcat(gamePathDisabled, ".disabled");
+
+    if (rename(gamePath, gamePathDisabled) < 0)
+    {
+        debug(0, "Failed to disable the controller, does it exist and is it already disabled?\n");
+        return JVS_CLI_STATUS_ERROR;
+    }
+    debug(0, "OpenJVS has disabled the controller '%s'.\n", deviceName);
+    return JVS_CLI_STATUS_SUCCESS_CLOSE;
+}
+
+JVSCLIStatus printListing()
+{
+    DeviceList deviceList;
+    if (!getInputs(&deviceList))
+    {
+        debug(0, "OpenJVS Failed to detect any controllers.\nMake sure you are running as root.\n");
+        return EXIT_FAILURE;
+    }
+    debug(0, "OpenJVS can detect the following controllers:\n\n");
+    InputMappings inputMappings;
+    inputMappings.length = 0;
+    debug(0, "Enabled:\n");
+    for (int i = 0; i < deviceList.length; i++)
+    {
+        char disabledString[MAX_PATH_LENGTH];
+        strcpy(disabledString, deviceList.devices[i].name);
+        strcat(disabledString, ".disabled");
+        int enabled = parseInputMapping(deviceList.devices[i].name, &inputMappings) == JVS_CONFIG_STATUS_SUCCESS;
+        if (enabled)
+        {
+            printf("  - %s\n", deviceList.devices[i].name);
+        }
+    }
+    debug(0, "\nDisabled:\n");
+    for (int i = 0; i < deviceList.length; i++)
+    {
+        char disabledString[MAX_PATH_LENGTH];
+        strcpy(disabledString, deviceList.devices[i].name);
+        strcat(disabledString, ".disabled");
+        int disabled = parseInputMapping(disabledString, &inputMappings) == JVS_CONFIG_STATUS_SUCCESS;
+        if (disabled)
+        {
+            printf("  - %s\n", deviceList.devices[i].name);
+        }
+    }
+    debug(0, "\nNo Mapping Present:\n");
+    for (int i = 0; i < deviceList.length; i++)
+    {
+        char disabledString[MAX_PATH_LENGTH];
+        strcpy(disabledString, deviceList.devices[i].name);
+        strcat(disabledString, ".disabled");
+        int enabled = parseInputMapping(deviceList.devices[i].name, &inputMappings) == JVS_CONFIG_STATUS_SUCCESS;
+        int disabled = parseInputMapping(disabledString, &inputMappings) == JVS_CONFIG_STATUS_SUCCESS;
+        if (!enabled && !disabled)
+        {
+            printf("  - %s\n", deviceList.devices[i].name);
+        }
+    }
+    return JVS_CLI_STATUS_SUCCESS_CLOSE;
+}
+
+JVSCLIStatus parseArguments(int argc, char **argv, char *map)
+{
+
+    if (argc <= 1)
+    {
+        return JVS_CLI_STATUS_SUCCESS_CONTINUE;
+    }
+    else if (argv[1][0] != '-')
+    {
+        strcpy(map, argv[1]);
+        return JVS_CLI_STATUS_SUCCESS_CONTINUE;
+    }
+    else if (strcmp(argv[1], "--help") == 0)
+    {
+        return printUsage();
+    }
+    else if (strcmp(argv[1], "--version") == 0)
+    {
+        return printVersion();
+    }
+    else if (strcmp(argv[1], "--enable") == 0)
+    {
+        return enableDevice(argc < 3 ? 0 : argv[2]);
+    }
+    else if (strcmp(argv[1], "--disable") == 0)
+    {
+        return disableDevice(argc < 3 ? 0 : argv[2]);
+    }
+    else if (strcmp(argv[1], "--list") == 0)
+    {
+        return printListing();
+    }
+
+    debug(0, "Unknown argument %s\n", argv[1]);
+    return JVS_CLI_STATUS_ERROR;
+}
