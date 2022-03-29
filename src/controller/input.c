fdfe1545a161df73e413bf7220da43315791472b
Bobby Dilley <bobby@netcraft.com>
2022-03-29 12:23:37 +0100

    Remove warning

--- src/controller/input.c
+++ src/controller/input.c
@@ -457,7 +457,7 @@ int processMappings(InputMappings *inputMappings, OutputMappings *outputMappings
         if (inputMappings->mappings[i].type == CARD)
         {
             evInputs->key[inputMappings->mappings[i].code] = tempMapping;
-            evInputs->abs[inputMappings->mappings[i].code].type = COIN;
+            evInputs->abs[inputMappings->mappings[i].code].type = (InputType)COIN;
             evInputs->absEnabled[inputMappings->mappings[i].code] = 1;
         }
     }
